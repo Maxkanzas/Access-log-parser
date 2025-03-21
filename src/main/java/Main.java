4e0eedba97fc6004ae2d86c9d76d292625e04f81
MKantselyaristov <maxkanz1991@gmail.com>
2025-03-21 09:49:39 +0300

    change Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1:  " + Math.random());
+        // Создаем объект Scanner для чтения ввода из консоли
+        Scanner scanner = new Scanner(System.in);
+
+        // Получаем первое число
+        System.out.print("Введите первое число: ");
+        int firstNumber = scanner.nextInt();
+
+        // Получаем второе число
+        System.out.print("Введите второе число: ");
+        int secondNumber = scanner.nextInt();
+
+        // Вычисляем сумму, разность, произведение и частное
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+
+        // Выводим результаты в консоль
+        System.out.println("Сумма: " + sum);
+        System.out.println("Разность: " + difference);
+        System.out.println("Произведение: " + product);
+        System.out.println("Частное: " + quotient);
     }
 }
